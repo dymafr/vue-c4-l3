aec52fd433e77aec69b1589f87c77aa62a8419c6
dymafr <erwan1309@hotmail.com>
2022-02-20 13:01:06 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <h1>Bonjour {{ user.prenom }}</h1>
-  <h1>Compteur : {{ state.compteur }}</h1>
-  <button @click="incCompteur()">+1</button>
+  <h1>Compteur : {{ compteur }}</h1>
+  <button @click="incCompteur">+1</button>
 </template>
 
 <script setup lang="ts">
-import { reactive, nextTick } from 'vue';
+import { ref } from 'vue';
 
-const state = reactive({
-  user: { prenom: 'Jean', age: 21 },
-  compteur: 0,
-});
-
-let {user} = state;
+const compteur = ref(0);
 
 function incCompteur() {
-  user.prenom = user.prenom === 'Jean' ? 'Paul' : 'Jean';
-  state.compteur++;
-  nextTick(() => {
-    console.log('Tick');
-  });
+  compteur.value++;
 }
 </script>
 
