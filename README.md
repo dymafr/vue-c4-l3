0bed5290f122ea4989c25549737bb2be22fed2f2
dymafr <erwan1309@hotmail.com>
2022-02-20 12:04:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-chduj8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-chduj8)
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Bonjour {{ user.prenom }}</h1>
+  <h1>Compteur : {{ state.compteur }}</h1>
+  <button @click="incCompteur()">+1</button>
+</template>
+
+<script setup lang="ts">
+import { reactive, nextTick } from 'vue';
+
+const state = reactive({
+  user: { prenom: 'Jean', age: 21 },
+  compteur: 0,
+});
+
+let {user} = state;
+
+function incCompteur() {
+  user.prenom = user.prenom === 'Jean' ? 'Paul' : 'Jean';
+  state.compteur++;
+  nextTick(() => {
+    console.log('Tick');
+  });
+}
+</script>
+
+<style></style>
